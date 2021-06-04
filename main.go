85d735295fabef455ce250357e4cb6c787b99c31
NickP0is0n <nickchaywork@gmail.com>
2021-06-04 23:25:20 +0300

    Added support for 2020 Edition, added legacy mode

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"flag"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -12,16 +13,31 @@ import (
 )
 
 var (
-	steamUrl   = "http://steamcommunity.com/games/1029630/members?p="
-	eversimUrl = "http://eversim.com/_geol/_geolreg_p19_sr.php"
-	re         = regexp.MustCompile("steamcommunity.com/profiles/(.*)'")
-	keyFound   = false
-	bytes      []byte
+	steamUrl         = "http://steamcommunity.com/games/1379930/members?p="
+	eversimUrl       = "http://eversim.com/_geol/_geolreg_p20_sr.php"
+	legacySteamUrl   = "http://steamcommunity.com/games/1029630/members?p="
+	legacyEversimUrl = "http://eversim.com/_geol/_geolreg_p19_sr.php"
+	re               = regexp.MustCompile("steamcommunity.com/profiles/(.*)'")
+	keyFound         = false
+	legacyMode       = true
+	bytes            []byte
 )
 
 func main() {
+	legacyModePtr := flag.Bool("legacy", false, "Enables legacy mode for 2019 Edition activation")
+	flag.Parse()
+	legacyMode = *legacyModePtr
+	if legacyMode {
+		fmt.Println("pnrGetID is running in legacy mode. Please be aware that legacy mode activates only 2019 Edition of Power and Revolution.")
+	}
+
 	fmt.Println("Start server...")
-	http.HandleFunc("/_geol/_geolreg_p19_sr.php", geolreg)
+	if legacyMode {
+		http.HandleFunc("/_geol/_geolreg_p20_sr.php", geolreg)
+	} else {
+		http.HandleFunc("/_geol/_geolreg_p19_sr.php", geolreg)
+	}
+	
 	if err := http.ListenAndServe(":80", nil); err != nil {
 		log.Fatal(err)
 	}
@@ -62,7 +78,11 @@ func geolreg(w http.ResponseWriter, r *http.Request) {
 func geolregPost(id string, form url.Values) (body []byte, ok bool) {
 	form["v4"] = []string{"#SK" + id + "ZZZZZZZZZZ"}
 	form["v6"] = []string{id + "@steam."}
-	resp, err := http.PostForm(eversimUrl, form)
+	resp, err := http.PostForm(eversimUrl, form)		
+	if legacyMode {
+		resp, err = http.PostForm(legacyEversimUrl, form)
+	}
+	
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -80,6 +100,9 @@ func geolregPost(id string, form url.Values) (body []byte, ok bool) {
 
 func grabSteamIds(page int) (matches [][]string, ok bool) {
 	resp, err := http.Get(steamUrl + strconv.Itoa(page))
+	if legacyMode {
+		resp, err = http.Get(legacySteamUrl + strconv.Itoa(page))
+	}
 	if err != nil {
 		fmt.Println(err)
 		return
